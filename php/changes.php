bcbb89af072f06561e586d0a6ebb843f09f2d403
Nick Belzer <nickbelzer@gmail.com>
2015-11-28 21:06:00 +0100

    Added CSS styling to the changes.php page
    Worked on CSS styling for the changes.php page. A shown item is now color coded, a general item would have a green line before it while a warning would have an orange color before it.
    
    Also added in a top-bar where it says 'Roosterwijzigingen' and it gives you the option to change from today to tomorrow or the other way around. It disabled the button you are on now and uses the post method to store the day of the week. This so that we avoid having to set the 'leerlingId' as well when we submit the form to go to another page.

--- php/changes.php
+++ php/changes.php
@@ -7,11 +7,14 @@ date_default_timezone_set($config["default_timezone"]);
 //error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 error_reporting(E_ERROR);
 
+$today = date("w");
+$tomorrow = ($today + 1)%7;
+
 if (is_numeric($_GET["studentID"]) && strlen($_GET["studentID"]) === 6) {
-    if (isset($_GET["weekDay"]) && $_GET["weekDay"] < 7 && $_GET["weekDay"] > -1) {
-        $day = $_GET["weekDay"];
+    if (isset($_POST["weekDay"]) && $_POST["weekDay"] < 7 && $_POST["weekDay"] > -1) {
+        $day = $_POST["weekDay"];
     } else {
-        $day = date("w");
+        $day = $today;
     }
     $schedule_changes = new Schedule_Changes($_GET["studentID"], $day);
 }
@@ -21,18 +24,39 @@ if (is_numeric($_GET["studentID"]) && strlen($_GET["studentID"]) === 6) {
     <head>
         <meta charset="UTF-8">
         <title>Persoonlijke wijzigingen</title>
+        <!-- Foundation and custom stylesheets -->
+        <link rel="stylesheet" type="text/css" href="foundation/css/foundation.css">
+        <link rel="stylesheet" type="text/css" href="foundation/css/schedule-changes.css">
     </head>
     <body>
-        <h1>Roosterwijzigingen</h1>
-        <br />
+        <div class="top-bar">
+            <div class="row">
+                <div class="top-bar-left small-12 medium-6 columns" style="padding-left: 0px">
+                    <div class="title">Roosterwijzigingen</div>
+                </div>
+                <div class="top-bar-right">
+                    <form action="" method="post">
+                        <div class="button-group">
+                            <button name="weekDay" value="<?php echo $today; ?>" type="submit" class="small-6 columns
+                        button <?php if
+                            ($today == $day) echo "disabled"; ?>">Vandaag</button>
+                            <button name="weekDay" value="<?php echo $tomorrow; ?>" type="submit" class="small-6
+                            columns button <?php if
+                            ($tomorrow
+                              == $day) echo "disabled"; ?>">Morgen</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div clas="row" style="margin-top: 1em;">
         <?php
         if (isset($schedule_changes)) {
             try {
-                echo "<h2>Algemene roosterwijzigingen</h2><br />";
                 $general_changes = $schedule_changes->file_manager->schedule_reader->getGeneralChanges();
                 if (!empty($general_changes)) {
                     foreach ($general_changes as $general_change) {
-                        echo $general_change . "<br />";
+                        DisplayMessage($general_change, "general");
                     }
                 } else {
                     echo "Er zijn geen algemene roosterwijzigingen voor deze dag.";
@@ -41,7 +65,7 @@ if (is_numeric($_GET["studentID"]) && strlen($_GET["studentID"]) === 6) {
                 $specific_changes = $schedule_changes->file_manager->schedule_reader->getSpecificChanges();
                 if (!empty($specific_changes)) {
                     foreach ($specific_changes as $specific_change) {
-                        echo $specific_change . "<br />";
+                        DisplayMessage($specific_change, "specific");
                     }
                 } else {
                     echo "Je hebt geen persoonlijke roosterwijzigingen voor deze dag.";
@@ -50,12 +74,37 @@ if (is_numeric($_GET["studentID"]) && strlen($_GET["studentID"]) === 6) {
                 echo $e->getMessage();
             }
         } else {
-            echo "<p>Leerlingnummer niet gevonden.</p>";
+            DisplayMessage("Leerlingnummer niet gevonden.", "warning");
         }
+
+
         ?>
+        </div>
     </body>
 </html>
 <?php
+
+// DisplayMessage("today: " . $today . " tomorrow: " . $tomorrow . " day: " . $day . "<br />", "warning");
+
 if (isset($schedule_changes)) {
     $schedule_changes->mySQL->closeConnection();
+}
+
+/**
+ * Gebruikt echo om een bericht als messageItem (css) weer te geven.
+ *
+ * @param string $message Het bericht dat weergegeven moet worden.
+ * @param string $type De manier waarop het bericht weergegeven moet worden -> [general, specific, warning].
+ * */
+function DisplayMessage($message, $type)
+{
+    echo "
+<div class='row'>
+    <div class='messageItem'>
+        <div class='small-12 medium-offset-1 columns $type'>
+              $message
+        </div>
+    </div>
+</div>
+";
 }
